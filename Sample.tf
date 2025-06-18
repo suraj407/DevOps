647590de056786874919f8a89737e203a1addce0
Pradeep Gandla <pgandla@starrez.com>
2025-06-18 12:23:21 +0000

    Network + DB
    Signed-off-by: Pradeep Gandla <pgandla@starrez.com>

--- Sample.tf
+++ Sample.tf
@@ -23,3 +23,53 @@ resource "azurerm_storage_account" "prod_storage" {
  account_tier = "Standard"
  account_replication_type = "GRS"
 }
+resource "azurerm_mssql_server" "dev_db" {
+  name = dev_db
+  resource_group_name = azurerm_resource_group.dev.name
+  location = "East US"
+  version = "12.0"
+  administrator_login = "admin
+  administrator_login_password = "AdminPassword111"
+  minimum_tls_version = var.sql_tls
+}​
+resource "azurerm_mssql_server" "prod_db" {
+  name = prod_db
+  resource_group_name = azurerm_resource_group.prod.name
+  location = "East US"
+  version = "12.0"
+  administrator_login = "admin"
+  administrator_login_password = "AdminPassword222"
+  minimum_tls_version = var.sql_tls
+}​
+resource "azurerm_network_security_group" "dev" {
+  name                = "dev-sg"
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
+}
+resource "azurerm_network_security_group" "prod" {
+  name                = "prod-sg"
+  location            = azurerm_resource_group.prod.location
+  resource_group_name = azurerm_resource_group.prod.name
+}
+resource "azurerm_virtual_network" "network" {
+  name                = "infra-network"
+  location            = azurerm_resource_group.prod.location
+  resource_group_name = azurerm_resource_group.prod.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name             = "dev-subnet"
+    address_prefixes = ["10.0.1.0/24"]
+    security_group   = azurerm_network_security_group.dev.id
+  }
+
+  subnet {
+    name             = "prod-subnet"
+    address_prefixes = ["10.0.2.0/24"]
+    security_group   = azurerm_network_security_group.prod.id
+  }
+
+  tags = {
+    name = "network"
+  }
+}
